b8fc8564031f452fd7514230cd7a491da6b32feb
Mila Neves <mila.neves@live.com>
2022-04-13 11:48:33 -0300

    feat: Create Project

---
+++ NevesGames.Application/Services/GamesServices.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using NevesGames.InfraData.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NevesGames.Application.Services
+{
+    public class GamesServices
+    {
+
+        private readonly IMongoCollection<Games> _GamesCollection;
+
+        public GamesServices(
+            IOptions<MongoDbSettings> mongoDbSettings)
+        {
+            var mongoClient = new MongoClient(
+                mongoDbSettings.Value.ConnectionString);
+
+            var mongoDatabase = mongoClient.GetDatabase(
+                mongoDbSettings.Value.DatabaseName);
+
+            _GamesCollection = mongoDatabase.GetCollection<Games>(
+                mongoDbSettings.Value.CollectionName);
+        }
+
+        public async Task<List<Games>> GetAsync() =>
+            await _GamesCollection.Find(_ => true).ToListAsync();
+
+        public async Task<Games?> GetAsync(string id) =>
+            await _GamesCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+        public async Task CreateAsync(Games newGames) =>
+            await _GamesCollection.InsertOneAsync(newGames);
+
+        public async Task UpdateAsync(string id, Games updatedGames) =>
+            await _GamesCollection.ReplaceOneAsync(x => x.Id == id, updatedGames);
+
+        public async Task RemoveAsync(string id) =>
+            await _GamesCollection.DeleteOneAsync(x => x.Id == id);
+    }
+}
---
+++ NevesGames.InfraData/Models/Games.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+
+namespace NevesGames.InfraData.Models
+{
+    public class Games
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; } 
+        public string Genre { get; set; }
+        public string Classification { get; set; }
+        public string Oringinal_Language { get; set; }
+
+    }
+}
